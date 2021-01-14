eeeedc08b7391534dee5c7be1adb628ff5f92ec8
SpaceOpossum <45968790+CharlieSpEx@users.noreply.github.com>
2021-01-14 20:16:37 +0100

    Add files via upload
    Completed version with comments.

--- receiver/port.c
+++ receiver/port.c
@@ -1,3 +1,13 @@
+/*--------------------------------------------------------------------------------------------------------
+Authors: Cezary Szczepañski, Pawe³ Religa
+Subcject: Techinka Mikroprocesorowa 2
+University: AGH Univerity of Science and Technology
+Project: Morse code reciever with decoding
+Section: Library for port initialization
+---------------------------------------------------------------------------------------------------------*/
+
+
+
 #include "MKL05Z4.h"
 #include "port.h"
 
@@ -5,8 +15,9 @@ void port_Init(void){
 	
 	SIM->SCGC5 |=  SIM_SCGC5_PORTB_MASK;
 	PORTB->PCR[1] |= PORT_PCR_MUX(1);
-	PORTB->PCR[1] |= PORT_PCR_IRQC(11); // 9 - rosnace, 10 - opadajace 12 - logic, 11 - oba
-	NVIC_ClearPendingIRQ(PORTB_IRQn);				/* Clear NVIC any pending interrupts on PORTC_B */
+	PORTB->PCR[1] |= PORT_PCR_IRQC(10);				// 9 - rosnace, 10 - opadajace 12 - logic, 11 - oba
+	NVIC_SetPriority(PORTB_IRQn, 2);
+	NVIC_ClearPendingIRQ(PORTB_IRQn);				
 	NVIC_EnableIRQ(PORTB_IRQn);
 	
 }
--- receiver/port.h
+++ receiver/port.h
@@ -1,3 +1,11 @@
+/*--------------------------------------------------------------------------------------------------------
+Authors: Cezary Szczepañski, Pawe³ Religa
+Subcject: Techinka Mikroprocesorowa 2
+University: AGH Univerity of Science and Technology
+Project: Morse code reciever with decoding
+Section: Library for port initialization
+---------------------------------------------------------------------------------------------------------*/
+
 #include "MKL05Z4.h"
 
 void port_Init(void);
