2d09c07b26f74e3e93906c013d7bcb8514c68118
SpaceOpossum <45968790+CharlieSpEx@users.noreply.github.com>
2021-01-14 20:09:43 +0100

    Add files via upload
    Working prototype, with comments.

--- receiver/main.c
+++ receiver/main.c
@@ -1,12 +1,26 @@
 /*--------------------------------------------------------------------------------------------------------
-					Technika Mikroprocesorowa 2 - laboratorium
-					Lab 5 - Ćwiczenie 4: wyzwalanie sprzętowe przetwornika A/C - tryb wyzwalania na żądanie (PIT0)
-					autor: Mariusz Sokołowski
-					wersja: 07.11.2020r.
+Authors: Cezary Szczepański, Paweł Religa
+Subcject: Techinka Mikroprocesorowa 2
+University: AGH Univerity of Science and Technology
+Project: Morse code reciever with decoding
 ---------------------------------------------------------------------------------------------------------*/
+
+
+/*
+					REALIZACJA PROGRAMU
+	Kod Morse'a dla każdego znaku przypisuję serię '.' i '-'. Dodatkowo wykorzystuje stałą czasową, dla rozróżnienia
+	tych dwóch symboli, a także dla wyznaczenia kolejnego symbolu oraz nadawanego wyrazu.	
+	Czas trwania symboli:
+		- '.': jedna stała czasowa
+		- '-': trzy stałe czasowe
+		- przerwa między nadawanymi symbolami: jedna stała czasowa
+		- przerwa między nadawanymi literami: trzy stałe czasowe
+		- przerwa między nadawanymi wyrazami: siedem stałych czasowych
+	
+	Dodatkowo każdy nadawany symbol, ma nie więcej niż 5 znaków.
+*/
 					
 #include "MKL05Z4.h"
-#include "ADC.h"
 #include "pit.h"
 #include "frdm_bsp.h"
 #include "lcd1602.h"
@@ -14,40 +28,55 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-//#include "uart0.h"
 #define TIME_UNIT BUS_CLOCK/10
 
-//float adc_volt_coeff = ((float)(((float)2.91) / 4095) );			// Współczynnik korekcji wyniku, w stosunku do napięcia referencyjnego przetwornika
 uint8_t wynik_ok = 0;
 uint16_t temp;
-uint16_t	wynik;
-int recieveFlag = 0;
-static int minDotCnt = 20;																			// zmienne do rozróżniania . od - na podstawie zliczonych impulsów
-static int maxDotCnt = 40;
+uint16_t wynik;
 int sampling_on = 0;
-uint16_t mCnt = 0;																			// wewnętrzny licznik do zliczania wywołań ADC
+uint16_t mCnt = 0;																				
 uint16_t tempCnt = 0;
-//static char recChar;
 char recSym[] = {'0','0','0','0','0'};
 static int wordCnt = 0;
+//													DEKLARACJA TABLICY DO DEKODOWANIA kodu Morse'a
 char morseTab[] = {'5','H','4','S','-','V','3','I','-','F','-','U','-','-','2','E','-','L','-','R','-','-','-','A','-','P','-','W','-','J','1','6','B','-','D','-','X','-','N','-','C','-','K','-','Y','-','T','7','Z','-','G','-','Q','-','M','8','-','-','O','9','-','0'};
 char recWord[] = {0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20};
+char display[]={0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20};
+char recChar = 'x';
 
-	char display[]={0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20};
-char recChar = 'k';
-void morseDecoder(char word[5]);	
 int symCnt = 0;
 uint64_t bool_map = 0;
 uint64_t bool_map_temp = 0;
 int position = 0;
-	int zero_cnt=0;
-	int translate_flag = 0;
-	int break_flag = 0;
-	int ones_cnt = 0;
-
+int zero_cnt=0;
+int translate_flag = 0;
+int break_flag = 0;
+int ones_cnt = 0;
+int space_flag = 0;
 static int index = 0;	
+
+void morseDecoder(char word[5]);
+
+void PORTB_IRQHandler() {
+													//	Obsługa przerwań GPIO na portB1. Mikrofon rejestrując sygnał, zmienia stan z wysokiego (1.5V) na niski. Każdorazowe takie zbocze 
+													//	opadające jest zliczane w zmiennej mCnt. 
+
+	if (sampling_on == 0) {							//	Funkcja do obsługi próbkowania sygnału z mikrofonu. Włącza licznik PIT gdy pojawi się pierwsze zbocze na portB.
+		sampling_on = 1;
+		PIT->CHANNEL[0].TCTRL |= PIT_TCTRL_TEN_MASK;
+	}
+
+	mCnt++;
+
+	PORTB->PCR[1] |= PORT_PCR_IRQC_MASK;			//	Wykasowanie flagi żądania przerwania.
+
+}
+
+
 void PIT_IRQHandler(){
-	
+													//	Licznik PIT, wywoływany po 1 jednostce czasowej. Sprawdza ile razy wystąpiły zbocza opadające w sygnale od mikrofonu od ostatniego 
+													//	wywołania PIT. Jeśli mikrofon zanotował sygnał, ustawia bit w rejestrze bool_map na wysoki, jeśli nie zostawia w stanie niskim.
+													//	Kolejne wywołania, ustawiają kolejne bity.
 	int tempCnt = mCnt;
 	mCnt = 0;
 	
@@ -57,12 +86,15 @@ void PIT_IRQHandler(){
 	} else {
 		zero_cnt++;
 	}
-	if(zero_cnt == 3){
-		translate_flag = 1;
+	if(zero_cnt == 3){								//	Jeśli wystąpiły 3 zera, oznacza to że zakończono nadawanie jednej litery alfabetu.
+		translate_flag = 1;							//	Ustawienie flagi wyzwalającej tłumaczenie.
 		break_flag = position;
 		bool_map_temp = bool_map;
 		bool_map = 0;
 		position = -1;
+	}
+	if(zero_cnt == 7){								// Wystąpienie 7 zer, oznacza przerwę między wyrazową w nadawnym zdaniu
+		space_flag = 1;
 	}
 	position++;
 	
@@ -74,6 +106,10 @@ void PIT_IRQHandler(){
 
 
 void morseDecoder(char word[5]){
+													//	Algorytm dekodowania został stworzony na podstawie schematu: https://en.wikipedia.org/wiki/Morse_code#/media/File:Morse_code_tree3.svg
+													//	Zbudowano do tego specjalną tbalicę morseTab. Funkcja polega na poruszaniu się po owej tablicy. Indexy są odpwiednio zwiększane 
+													//	lub zmniejszane, zależnie od tego jaki znak jest w przekazanym słowie. 
+
 	int i = 0;
 	if (word[0] == '-') i += 47; else if (word[0] == '.')i += 16;
 	if (word[1] == '-') i += 8; else if (word[1] == '.') i -= 8; else recChar =  morseTab[i-1];
@@ -86,38 +122,22 @@ void morseDecoder(char word[5]){
 
 
 
-uint16_t status = 0;
-
-void PORTB_IRQHandler(){
-	
-	
-		if(sampling_on == 0){
-			sampling_on = 1;
-			PIT->CHANNEL[0].TCTRL |= PIT_TCTRL_TEN_MASK;
-		}
-	
-		mCnt++;
-	
-		PORTB->PCR[1] |= PORT_PCR_IRQC_MASK;
-
-}
 
 int main (void)
 {
-//	uint32_t i=0;
-	LCD1602_Init();		 																		// Inicjalizacja wyświetlacza LCD
+	LCD1602_Init();		 							// Inicjalizacja wyświetlacza LCD
+	PIT_Init();										// Inicjalizacja licznika PIT0
+	port_Init();									// Inicjalizacja portów
 	LCD1602_Backlight(TRUE);
-	LCD1602_Print("xx");																	// Ekran kontrolny
-	PIT_Init();																					  // Inicjalizacja licznika PIT0
-	port_Init();																					// Inicjalizacja portów
+	LCD1602_Print("xx");							// Ekran kontrolny
 	LCD1602_SetCursor(0,1);
 	LCD1602_Print("--");
 	
 
 	while(1)
 	{
-		if(translate_flag){
-			translate_flag = 0;
+		if(translate_flag){							//	Wywołanie warunku oznacza, że wystąpiła przerwa między nadawanymi znakami. 
+			translate_flag = 0;						//	Na podstawie zawartości rejestru bool_map pętla while rozpoznaje znak jako '.' lub '-', i zapisuje znak do recSym.
 			ones_cnt = 0;
 			for(int i = 0; i < break_flag; i++){
 				if(bool_map_temp & 1<<i){
@@ -132,20 +152,23 @@ int main (void)
 				}
 			}
 			
-			morseDecoder(recSym);
-			recWord[wordCnt++] = recChar;
+			morseDecoder(recSym);					//	Po rozpoznaniu zawartości rejestru bool_map i rozdzieleniu na odpowiednie znaki,
+			recWord[wordCnt++] = recChar;			//	zarejstrowane znaki są przekazywane do tłumaczenia na litery alfabetu.
 			
 			LCD1602_SetCursor(0,0);
 			LCD1602_Print(recSym);
-			sprintf(display,"%d",bool_map_temp);
 			
 			for(int i = 0; i < 5; i++){recSym[i] = '0';}
 			symCnt = 0;
 			
 			LCD1602_SetCursor(0,1);
-			LCD1602_Print(recWord);
+			LCD1602_Print(recWord);					//	Wysyłanie odczytanej litery na ekran.
 		}
-
+		
+		if(space_flag){								//	Rozpoznanie i wyświetlenie spacji w odebranym zdaniu.
+			space_flag = 0;
+			recWord[wordCnt++] = ' ';
+		}
 		
 	}
 }
