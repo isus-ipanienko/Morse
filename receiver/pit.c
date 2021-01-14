e87238d8316aae7a43ddf2e3432c205c06a4eaf6
SpaceOpossum <45968790+CharlieSpEx@users.noreply.github.com>
2021-01-14 20:16:12 +0100

    Add files via upload
    Completed version with comments.

--- receiver/pit.c
+++ receiver/pit.c
@@ -1,24 +1,26 @@
+/*--------------------------------------------------------------------------------------------------------
+Authors: Cezary Szczepański, Paweł Religa
+Subcject: Techinka Mikroprocesorowa 2
+University: AGH Univerity of Science and Technology
+Project: Morse code reciever with decoding
+Section: Library for PIT initialization
+---------------------------------------------------------------------------------------------------------*/
+
 #include "MKL05Z4.h"
 #include	"pit.h"
 
-#define TIME_UNIT 2477500
+#define TIME_UNIT 2477500									//	Stała czasowa wykorzystywana do transmisji i odbioru kodu Morse'a
 
 
 void PIT_Init(void)
 {
-	uint32_t tsv;
-	SIM->SCGC6 |= SIM_SCGC6_PIT_MASK;			// Włączenie sygnału zegara do modułu PIT
-	PIT->MCR &= ~PIT_MCR_MDIS_MASK;				// Włączenie modułu PIT
+	uint32_t tsv;	
+	SIM->SCGC6 |= SIM_SCGC6_PIT_MASK;						//	Włączenie sygnału zegara do modułu PIT
+	PIT->MCR &= ~PIT_MCR_MDIS_MASK;							//	Włączenie modułu PIT
 	
-	PIT->CHANNEL[0].LDVAL = PIT_LDVAL_TSV(TIME_UNIT);	  //czas między znakami
+	PIT->CHANNEL[0].LDVAL = PIT_LDVAL_TSV(TIME_UNIT);		//	Czas między znakami
 	PIT->CHANNEL[0].TCTRL |= PIT_TCTRL_TIE_MASK;
-	
-	//PIT->CHANNEL[1].LDVAL = PIT_LDVAL_TSV(TIME_UNIT*3);	//czas między znakami
-	//PIT->CHANNEL[1].TCTRL |= PIT_TCTRL_TIE_MASK;
-	
-	//PIT->CHANNEL[2].LDVAL = PIT_LDVAL_TSV(TIME_UNIT*7);	//czas między znakami
-	//PIT->CHANNEL[2].TCTRL |= PIT_TCTRL_TIE_MASK;
-	
+
 	NVIC_SetPriority(PIT_IRQn, 3);
 	NVIC_ClearPendingIRQ(PIT_IRQn);
 	NVIC_EnableIRQ(PIT_IRQn);	
--- receiver/pit.h
+++ receiver/pit.h
@@ -1,3 +1,11 @@
+/*--------------------------------------------------------------------------------------------------------
+Authors: Cezary Szczepañski, Pawe³ Religa
+Subcject: Techinka Mikroprocesorowa 2
+University: AGH Univerity of Science and Technology
+Project: Morse code reciever with decoding
+Section: Library for PIT initialization
+---------------------------------------------------------------------------------------------------------*/
+
 #ifndef pit_h
 #define pit_h
 #define BUS_CLOCK SystemCoreClock/2
